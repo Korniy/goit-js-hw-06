2bd1a612e1e4e549e6a84519fd77bbc03d003ee8
Volodymyr Korniychuk <korniychuk49@gmail.com>
2022-06-28 17:01:58 +0300

    commit

---
+++ js/task-01.js
@@ -0,0 +1,7 @@
+const listEl = document.querySelector("#categories");
+const numberOfElements = listEl.children.length;
+console.log("Number of categories:", numberOfElements);
+
+const itemEl = document.querySelector(".item");
+const titleEl = itemEl.children.length;
+console.log("Category :", titleEl)
