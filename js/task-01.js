248b3bba8aa4be17cd9435fdde492645f307842e
Volodymyr Korniychuk <korniychuk49@gmail.com>
2022-06-28 18:21:39 +0300

    Виконує 1 завдання

--- js/task-01.js
+++ js/task-01.js
@@ -1,7 +1,55 @@
+
+// 1
+
 const listEl = document.querySelector("#categories");
 const numberOfElements = listEl.children.length;
 console.log("Number of categories:", numberOfElements);
 
-const itemEl = document.querySelector(".item");
-const titleEl = itemEl.children.length;
-console.log("Category :", titleEl)
+// 2
+
+const itemEl = document.querySelectorAll('.item');
+itemEl.forEach.call(itemEl, (element) => {
+    const titleEl = element.querySelector('h2').innerHTML;
+    console.log("Category:", titleEl);
+    
+    const itemsLength = element.querySelectorAll('li').length;
+    console.log("Elements:",itemsLength )
+    
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
