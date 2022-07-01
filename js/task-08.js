bc52b2195c3356b314db4aa2952ac7c6efefeced
Volodymyr Korniychuk <korniychuk49@gmail.com>
2022-07-01 20:42:34 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
+const loginFormEl = document.querySelector(".login-form");
+
+loginFormEl.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+     const emailEl = event.currentTarget.elements.email.value;
+     const passwordEl = event.currentTarget.elements.password.value;
+
+  if (emailEl === "" || passwordEl === "") {
+    return alert("Заповніть всі поля");
+    } 
+    
+    const object = { emailEl, passwordEl,};
+    console.log(object);
+  
+    return loginFormEl.reset();
+});
+
+
+  
+
+ 
+
+
 
